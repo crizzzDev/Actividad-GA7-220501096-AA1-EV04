88d91a46e66bf8675b0bac5d41c4ef0067f9d581
Cristian Montoya <Cristian.montoya63@soysena.edu.co>
2024-05-04 23:49:21 -0500

    Cambios en HTML, CSS Y Js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// JavaScript para interacciones dinámicas
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JavaScript aquí
+    console.log('El DOM ha sido cargado');
+});
